b21f8be702a841626d1016454723c73c702f3831
BracketJohn <njonalik@protonmail.com>
2022-10-12 15:35:28 +0200

    cleanup session logic a bit more, polish rweadme for first publish

--- src/runtime/session/index.ts
+++ src/runtime/session/index.ts
@@ -1,12 +1,10 @@
 import { H3Event, defineEventHandler, setCookie, parseCookies } from 'h3'
 import { nanoid } from 'nanoid'
-import type { CookieSerializeOptions } from 'cookie-es'
-import { StorageValue } from 'unstorage'
 import dayjs from 'dayjs'
 import useConfig from './config'
 import { getStorageSession, setStorageSession } from './storage'
 
-const safeSetCookie = (event: H3Event, name: string, value: string, optionOverwrite: Partial<CookieSerializeOptions> = {}) => setCookie(event, name, value, {
+const safeSetCookie = (event: H3Event, name: string, value: string) => setCookie(event, name, value, {
   // Max age of cookie in seconds
   maxAge: useConfig().sessionExpiryInSeconds,
   // Only send cookie via HTTPs to mitigate man-in-the-middle attacks
@@ -14,7 +12,7 @@ const safeSetCookie = (event: H3Event, name: string, value: string, optionOverwr
   // Only send cookie via HTTP requests, do not allow access of cookie from JS to mitigate XSS attacks
   httpOnly: true,
   // Do not send cookies on many cross-site requests to mitigates CSRF and cross-site attacks, see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite#lax
-  sameSite: useConfig().sessionCookieSameSite,
+  sameSite: useConfig().sessionCookieSameSite
 })
 
 declare interface Session {
@@ -22,28 +20,37 @@ declare interface Session {
   createdAt: Date
 }
 
-const getSessionId = (event: H3Event) => parseCookies(event).sessionId
-const ensureSessionId = (event: H3Event) => {
-  const existingSessionId = getSessionId(event)
-  if (existingSessionId) {
-    return existingSessionId
-  }
+const getEventSessionId = (event: H3Event) => parseCookies(event).sessionId
+const getEventSession = (event: H3Event) => event.context.session
 
-  const newSessionId = nanoid(useConfig().sessionIdLength)
-  safeSetCookie(event, 'sessionId', newSessionId)
+const newSession = async (event: H3Event) => {
+  const sessionId = nanoid(useConfig().sessionIdLength)
 
-  return newSessionId
-}
+  // Set cookie
+  safeSetCookie(event, 'sessionId', sessionId)
 
-const setEventSession = (event: H3Event, session: StorageValue) => {
-  event.context.session = session
+  // Store session data in storage
+  const session: Session = { id: sessionId, createdAt: new Date() }
+  await setStorageSession(sessionId, session)
+
   return session
 }
-const useEventSession = (event: H3Event) => event.context.session
 
-const newSession = (id: string): Session => ({ id, createdAt: new Date() })
+const getSession = async (event: H3Event): Promise<null | Session> => {
+  const existingSessionId = getEventSessionId(event)
+  if (!existingSessionId) {
+    return null
+  }
 
-function isSession(shape: unknown): shape is Session {
+  const session = await getStorageSession(existingSessionId)
+  if (!isSession(session)) {
+    return null
+  }
+
+  return session
+}
+
+function isSession (shape: unknown): shape is Session {
   if (typeof shape === 'object' && !!shape && 'id' in shape && 'createdAt' in shape) {
     return true
   }
@@ -52,35 +59,25 @@ function isSession(shape: unknown): shape is Session {
 }
 
 const ensureSession = async (event: H3Event) => {
-  const sessionId = ensureSessionId(event)
-
-  let session: Session
-
-  const existingSession = await getStorageSession(sessionId)
-  if (isSession(existingSession)) {
-    session = existingSession
-  } else {
-    session = newSession(sessionId)
+  let session: null | Session = await getSession(event)
+  if (!session) {
+    session = await newSession(event)
   }
 
-  // Start a new session if session is expired (read: too old)
+  // Start a new session if session is expired (aka: too old)
   const now = dayjs()
   if (now.diff(dayjs(session.createdAt), 'seconds') > useConfig().sessionExpiryInSeconds) {
-    session = newSession(sessionId)
+    session = await newSession(event)
   }
 
-  setStorageSession(sessionId, session)
-  setEventSession(event, session)
+  event.context.session = session
+  return session
 }
 
 export default defineEventHandler(async (event: H3Event) => {
-  console.log('in handl;er')
-
   // 1. Ensure that a session is present by either loading or creating one
   await ensureSession(event)
 
-  console.log('heeey', event.context.session)
-
   // 2. Setup a hook that saves any changed made to the session by the subsequent endpoints
-  event.res.on('finish', async () => await setStorageSession(getSessionId(event), useEventSession(event)))
+  event.res.on('finish', async () => await setStorageSession(getEventSessionId(event), getEventSession(event)))
 })
