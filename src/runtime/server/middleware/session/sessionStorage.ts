66e2d8c4e36571167a3a5f54e57a6e696cbf722a
Kaleb Ercanbrack <twitcherc@gmail.com>
2022-10-28 09:12:32 -0600

    fix options not getting used by driver

--- src/runtime/server/middleware/session/sessionStorage.ts
+++ src/runtime/server/middleware/session/sessionStorage.ts
@@ -1,5 +1,5 @@
 import { createStorage } from 'unstorage'
 import sessionDriver from '#session-driver'
 import sessionConfig from '#session-config'
-
-export const sessionStorage = createStorage({ driver: sessionDriver() }).mount(sessionConfig.session.storePrefix, sessionDriver())
+const driver = sessionDriver(sessionConfig.session.storageOptions.options)
+export const sessionStorage = createStorage({ driver }).mount(sessionConfig.session.storePrefix, driver)
