8238d43b6c2b934300be1041b78fef8d3100f658
Sebastian Rager <mail@interpretor.net>
2022-12-05 17:08:43 +0100

    Rename variable

--- src/runtime/server/middleware/session/resEndProxy.ts
+++ src/runtime/server/middleware/session/resEndProxy.ts
@@ -4,11 +4,11 @@ type MiddleWare = () => Promise<void>
 
 // Proxy res.end() to get a callback at the end of all event handlers
 export const resEndProxy = (res: ServerResponse, middleWare: MiddleWare) => {
-  const _end = res.end
+  const end = res.end
 
   // @ts-ignore Replacing res.end() will lead to type checking error
   res.end = async (chunk: any, encoding: BufferEncoding) => {
     await middleWare()
-    return _end.call(res, chunk, encoding) as ServerResponse
+    return end.call(res, chunk, encoding) as ServerResponse
   }
 }
