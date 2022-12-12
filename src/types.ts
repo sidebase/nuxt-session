04188463cc739068672aed74ee04dec638e8faf4
Sebastian Rager <mail@interpretor.net>
2022-12-12 13:48:26 +0100

    Fix indentation and comma

--- src/types.ts
+++ src/types.ts
@@ -104,7 +104,7 @@ export interface SessionOptions {
    * @example true
    * @type boolean
    */
-  rolling: boolean,
+  rolling: boolean
   /**
    * Forces a session that is "uninitialized" to be saved to the store. A session is uninitialized when it is new but not modified.
    * Choosing false is useful for implementing login sessions, reducing server storage usage, or complying with laws that require permission before setting a cookie.
@@ -112,15 +112,15 @@ export interface SessionOptions {
    * @example false
    * @type boolean
    */
-  saveUninitialized: boolean,
+  saveUninitialized: boolean
   /**
-    * Forces the session to be saved back to the session store, even if the session was never modified during the request.
-    * Depending on your store this may be necessary, but it can also create race conditions where a client makes two parallel requests to your server,
-    * and changes made to the session in one request may get overwritten when the other request ends, even if it made no changes (this behavior also depends on what store you're using).
-    * @default true
-    * @example false
-    * @type boolean
-    */
+   * Forces the session to be saved back to the session store, even if the session was never modified during the request.
+   * Depending on your store this may be necessary, but it can also create race conditions where a client makes two parallel requests to your server,
+   * and changes made to the session in one request may get overwritten when the other request ends, even if it made no changes (this behavior also depends on what store you're using).
+   * @default true
+   * @example false
+   * @type boolean
+   */
   resave: boolean
 }
 
