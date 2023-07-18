0b4b58b1f3af0e200c0189eab665b653bbc5700e
ryoji-yamauchi-blc <ryoji.yamauchi.ut@ambl.co.jp>
2023-07-18 16:35:22 +0900

    avoid prevent extensions

--- src/runtime/server/middleware/session/sessionStorage.ts
+++ src/runtime/server/middleware/session/sessionStorage.ts
@@ -3,6 +3,6 @@ import { useRuntimeConfig } from '#imports'
 // @ts-ignore
 import sessionDriver from '#session-driver'
 const sessionConfig = useRuntimeConfig().session.session
-const driver = sessionDriver(sessionConfig.storageOptions.options)
+const driver = sessionDriver({ ...sessionConfig.storageOptions.options })
 const storage = createStorage({ driver }).mount(sessionConfig.storePrefix, driver)
 export const sessionStorage = prefixStorage(storage, sessionConfig.storePrefix)
